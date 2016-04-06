efe05285bda6d5d277b63fe12bd662bd6433bbb7
Leland Jordon <lelandjordon@mac.com>
2016-04-06 18:53:12 -0400

    built an ordered deck and a shuffled deck with do|| after failing with .map

--- high_card.rb
+++ high_card.rb
@@ -3,3 +3,19 @@ ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
 players = []
+full_suits = []
+full_deck = []
+
+puts ranks
+puts suits
+puts "all card types noted"
+
+  suits.each do |suit|
+    ranks.each do |rank|
+      full_deck << [rank, suit]
+    end
+  end
+puts full_deck.inspect
+
+shuffled_deck = full_deck.shuffle
+puts shuffled_deck.inspect
